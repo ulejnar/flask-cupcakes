8f9acc0fa253ea43072490c3e3ff7e35cedfb81c
gladkill <gladkillmusic@gmail.com>
2020-03-30 12:42:51 -0700

    first two routes complete

--- app.py
+++ app.py
@@ -1,1 +1,48 @@
 """Flask app for Cupcakes"""
+from flask import Flask, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "abcdef"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
+
+def serialize_cupcake(cupcake):
+    return {"flavor": cupcake.flavor,
+            "size": cupcake.size,
+            "rating": cupcake.rating,
+            "image": cupcake.image
+            }
+
+
+@app.route('/api/cupcakes')
+def list_all():
+    """get listing of all cupcakes"""
+    cupcakes = Cupcake.query.all()
+    serialized = [serialize_cupcake(c) for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake_data(cupcake_id):
+
+    """get info about current cupcake"""
+    current_cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = serialize_cupcake(current_cupcake)
+
+    return jsonify(current_cupcake=serialized)
+
+   # inquire if this jsonify argument has to match our created variable
+
+
+
--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Cupcake app."""
+"""Models for adopt app."""
+
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.String,
+                       nullable=False)
+    size = db.Column(db.String,
+                     nullable=False)
+    rating = db.Column(db.Float,
+                       nullable=False)
+    image = db.Column(db.String,
+                      default='https://tinyurl.com/demo-cupcake')
